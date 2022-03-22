78a3aba7966f44332c1820426703c0f8c00ab7b1
AlejandroArroyo1999 <alejandroarroyo1999@gmail.com>
2022-03-22 17:53:37 +0100

    More methods added using the StringBuilder class, and the dimension method.

--- SliderPuzzle/Board.java
+++ SliderPuzzle/Board.java
@@ -4,6 +4,9 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Board {
     private int[][] square;
     private int n;
@@ -11,7 +14,7 @@ public class Board {
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        n = tiles.length;
+        this.n = tiles.length;
         this.square = new int[tiles.length][tiles[0].length];
         for (int i = 0, n = tiles.length; i < n; i++) {
             for (int j = 0; j < n; j++) this.square[i][j] = tiles[i][j];
@@ -20,12 +23,23 @@ public class Board {
 
     // string representation of this board
     public String toString() {
-        
+        StringBuilder result = new StringBuilder();
+        result.append(this.n + "\n");
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                result.append(this.square[i][j]);
+                result.append(" ");
+            }
+            result.append("\n");
+        }
+        return result.toString();
     }
 
     // board dimension n
-    public int dimension()
-
+    public int dimension() {
+        return this.n;
+    }
+    /*
     // number of tiles out of place
     public int hamming()
 
@@ -44,7 +58,18 @@ public class Board {
     // a board that is obtained by exchanging any pair of tiles
     public Board twin()
 
+     */
+
     // unit testing (not graded)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int len = in.readInt();
+        int[][] tiles = new int[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) tiles[i][j] = in.readInt();
+        }
+        Board result = new Board(tiles);
+        StdOut.println(result.toString());
+    }
 
 }
